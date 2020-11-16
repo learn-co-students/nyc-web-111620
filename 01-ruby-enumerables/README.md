c70a10959e3fc111c854ab258e99bc068ae88e39
Caryn McCarthy <cmccarthy15@gmail.com>
2020-11-16 16:56:35 -0500

    after 01 ruby intro lecture

--- 01-ruby-enumerables/README.md
+++ 01-ruby-enumerables/README.md
@@ -1,9 +1,9 @@
 # Ruby Enumerables & Pry
 
 ## Prerequisites // Questions
-- DataTypes:  
-- Conditionals 
-- Loops 
+- DataTypes: booleans (bools - truthy/falsey), hashes, strings, integers, arrays, float, nil, symbols,
+- Conditionals: if/elsif/else/end, case/when/end, until, do/while,. || && < > == ! !! (js in particular ===)
+- Loops: until, do/while, for, each, map, times and all ze methods that us loop over arrays and thangs! 
 - Arrays vs Hashes
 - Accessing Arrays & Accessing Hashes
 - Iterating through Arrays and Hashes using `.each`
@@ -11,17 +11,20 @@
 ## Agenda
 Command Line Application (CLI)
 
-- Mindset: 
+- Mindset: building your toolbox. which tool is best for each situation? 
 - Enumerables
     - .each
-    - .map && .collect
-    - .filter && .select 
-    - .find
+    - .map && .collect ====> going from an array of hashes to an array of strings for instance ( option 1 ) 
+    - .filter && .select && .find_all
+    - .find && .detect
+    - the *block parameter* needs to be logically & descriptively named!!! (the thing btwn the pipes)
 - Pry
 - Show where this code lives on github and how to access it! 
 
 
 ## Icebox / Addtnl Practice
+- make it all domesticated animals instead of finding just dogs (so dogs, cats, fish)
+- recursion is so beautiful!!!!!! (rabbit hole of doom/joy?)
 
 
 
--- 01-ruby-enumerables/run.rb
+++ 01-ruby-enumerables/run.rb
@@ -1,16 +1,64 @@
+require 'pry'
 # 1. I am building a Pets application. I need to a way to hold all the information (name, type) about a SPECIFIC pet. Which datatype should I use?
 # 2. Now, I need a way to hold a list of all of the pets in my app. Which datatype should I use? 
 # 3. I want a list of only the names of each pet. How do I do that? What datatype should be returned?
 # 4. I want to be to find all pets that are dogs. What iterator should I use? What datatype should be returned?
-# 5. I want to be able to find a single dog with a specific name. How do I do that? What datatype should be returned?
+# 5. I want to be able to find a single animal with a specific name. How do I do that? What datatype should be returned?
 
+# Array of Hashes  #AoH
 
+animals = [ 
+    {name: "turbo", species: "cat", sound: "meeeeooooooowwwwww"}, # how sshould i represent mojo
+    {name: "Cloud", species: "fish", sound: "gluuuuurp"},
+    {name: "Mickey", species: "dog", sound: "arf"},
+    {name: "Skip", species: "dog", sound: "woof"},
+    {name: "Bella", species: "dog", sound: "ruff"},
+    {name: 'turbo', species: 'dog', sound: 'woof'},
+    {name: 'Grizz', species:  'bear', sound: 'Grrrr'},
+    {name: "Sniffy", species: "mouse", sound: "sniff sniff"},
+    {name: "Nugget", species: "dog", sound: "woo"},
+    {name: "spark", species: "dog", sound: "grrrrr"},
+    {name: "Sharky", species: "shark", sound: "Help I’m in a tornado"},
+    {name: 'raffi', species: 'lion', sound:'rawr'},
+    {name: "Porky", species: "pig", sound: "oink"}
+]
 
 # 1. show a list of all the names of all the pets
 # 2. show me a list of just the pets that are... dogs?
 # 3. let me search for pet by name
 
+def run(animals_array) # this is just the definition 
+    puts "WELCOME TO THE PET STORE!!!"
+    puts "Would you like to..."
+    puts "1. See a list of all pet names?"
+    puts "2. See a list of all dogs?"
+    puts "3. Find an animal by name?"
+    choice = gets.chomp
+    if choice.chomp == "1"
+        # puts "you selected see all names"
+        # all_names = []
+        # animals_array.each { |animal| all_names << animal[:name] }
+        # all_names
+        p animals_array.map { |animal| animal[:name] } # or collect!!
+        
+    elsif choice.chomp == "2"
+        p animals_array.select { |animal| animal[:species] == "dog" }
+        # alternates are filter and find_all
+    elsif choice.chomp == "3"
+        puts "Which animal are you looking for?"
+        name = gets.chomp
+        p animals_array.find { |ani| ani[:name] == name } # or detect
+        binding.pry
+    else
+        puts "That was not a valid option. Please try again!"
+        run(animals_array)
+    end
 
-def run
-    puts "Application is running..."
+
 end
+
+
+run(animals) #invoke/call/run the method 
+
+binding.pry # when we run this file, will stop execution here and start repl
+# ways to exit are... exit, exit!, !!! (last two will exit out of ALL pry's)
