692d7462bc94d084a956e7147ea15ab9a7bf7eaf
Michelle Rios <brendamichelle995@gmail.com>
2021-01-11 15:46:02 -0500

    before lecture

---
+++ 22-dom-manipulation/index.js
@@ -0,0 +1,37 @@
+/*********** CRUD with the DOM ***********/
+// https://developer.mozilla.org/en-US/
+// https://www.w3schools.com/cssref/css_selectors.asp 
+
+// READ
+
+
+// UPDATE
+
+
+// DELETE
+
+
+// CREATE
+// TODO Create post for each object in this array
+let postsArray = [
+    {
+        meme: 'pics/dino.jpeg',
+        description: 'Ahhh!!!'
+
+    },
+    {
+        meme: 'pics/elvish.jpeg',
+        description: 'Hmm 🤔'
+
+    },
+    {
+        meme: 'pics/space.jpeg',
+        description: 'Have standards people, standards!!!'
+
+    },
+    {
+        meme: 'pics/restaurant.jpg',
+        description: 'Let\'s wine & dine!'
+
+    }
+]
