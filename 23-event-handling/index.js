d5a7059b68509e6346af43c296b6c4b352496573
Michelle Rios <brendamichelle995@gmail.com>
2021-01-11 16:34:36 -0500

    before lecture

---
+++ 23-event-handling/index.js
@@ -0,0 +1,22 @@
+let postsArray = [
+    {
+        meme: 'pics/dino.jpeg',
+        description: 'Ahhh!!!'
+
+    },
+    {
+        meme: 'pics/elvish.jpeg',
+        description: 'Hmm 🤔'
+
+    },
+    {
+        meme: 'pics/space.jpeg',
+        description: 'Have standards people, standards!!!'
+
+    },
+    {
+        meme: 'pics/restaurant.jpg',
+        description: 'Let\'s wine & dine!'
+
+    }
+]
