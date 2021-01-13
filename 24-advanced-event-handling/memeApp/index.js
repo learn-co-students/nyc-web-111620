e9347326313a89f092e4a68dee64765f63e6e8fb
Michelle Rios <brendamichelle995@gmail.com>
2021-01-13 12:20:30 -0500

    after lecture; organized - see note top of index.js

--- 24-advanced-event-handling/memeApp/index.js
+++ 24-advanced-event-handling/memeApp/index.js
@@ -1,6 +1,13 @@
+// This file contains the event delegation approach. I organized the code into their respective
+// section (DOM Elements vs Event Listeners). Search for STEP 1, STEP 2, and STEP 3 to find the code 
+// we used for this approach. Alternatively, you can find the unaltered code in the previous commit.
+// If you need help with that, send me a message
+
 /******************* DOM Elements ******************/
 const toggleSwitch = document.querySelector('#toggle-dark-mode')
 const memeForm = document.querySelector('#meme-form')
+// STEP 1: find common ancestor/parent
+const listings = document.querySelector('#listings')
 
 
 /******************* HELPER FUNCTIONS/EVENT HANDLERS *******************/
@@ -44,18 +51,9 @@ memeForm.addEventListener('submit', function (event) {
     renderOnePost(post)
 })
 
-
-/******************* INITIAL RENDER *******************/
-postsArray.forEach(function (postObject) {
-    renderOnePost(postObject)
-})
-
-// STEP 1: find common ancestor/parent
-const listings = document.querySelector('#listings')
-
 // STEP 2: Add event listener
 listings.addEventListener('click', function (event) {
-    // STEP 3: Add conditional to know if the event we
+    // STEP 3: Add conditional logic to know if the event we
     // care about was clicked
     const postDiv = event.target.closest('div.post')
 
@@ -72,6 +70,11 @@ listings.addEventListener('click', function (event) {
     }
 })
 
+
+/******************* INITIAL RENDER *******************/
+postsArray.forEach(function (postObject) {
+    renderOnePost(postObject)
+})
 
 /************** FLAWED APPROACH **************/
 
@@ -100,95 +103,4 @@ listings.addEventListener('click', function (event) {
 
 // document.body.addEventListener('click', function(event) {
 //     console.log('BODY CLICKED. ', event.target)
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**************** FROM DOM LECTURE ****************/
-
-//  Create a post for each object in the postsArray array
-// THIS IS HOW WE WANT EACH POST TO BE STRUCTURED
-// < div class="post" >
-//     <div class="image">
-//         <img src="pics/bugfix.jpeg">
-//     </div>
-//     <h4 class="description">Has this ever happened to you?</h4>
-// </div>
-
-
-
-/***************** ARTISANAL APPROACH *****************/
-// postsArray.forEach(function (postObject) {
-//     // STEP 1: Create each element
-//     const newOuterDiv = document.createElement('div')
-//     const newInnerDiv = document.createElement('div')
-//     const newImg = document.createElement('img')
-//     const newH4 = document.createElement('h4')
-
-//     // STEP 2: Add attributes to new elements!
-//     // newOuterDiv.classList.add('post')
-//     newOuterDiv.className = 'post'
-//     newOuterDiv.id = "someId"
-
-//     newInnerDiv.classList.add('image')
-//     newImg.src = postObject.memeUrl
-
-//     newH4.classList.add('description')
-//     newH4.textContent = postObject.description
-
-//     // STEP 3: Figure out the relationships, if any
-//     newOuterDiv.append(newInnerDiv, newH4)
-//     newInnerDiv.append(newImg)
-
-//     // STEP 4: Slap it on the DOM!
-//     // Where are we slapping our elements?? To which element are we adding/appending it?
-//     const listings = document.querySelector("#listings")
-//     listings.append(newOuterDiv)
-// })
-
-
-/***************** INNERHTML APPROACH *****************/
-// postsArray.forEach(function (postObject) {
-//     // STEP 1: Find where we want to add the new post
-//     const listings = document.querySelector('#listings')
-//    // STEP 2: update the innerHTML
-//     listings.innerHTML +=
-//         `<div class="post" >
-//             <div class="image">
-//                 <img src="${postObject.memeUrl}">
-//             </div>
-//             <h4 class="description">${postObject.description}</h4>
-//         </div>`
-// })
-
-/***************** INNERHTML/ARTISINAL COMBO APPROACH *****************/
-// postsArray.forEach(function (postObject) {
-//     // STEP 1: Find where we want to add the new post
-//     const listings = document.querySelector('#listings')
-
-//     // STEP 2: create the outermost element
-//     const outerDiv = document.createElement('div')
-//     outerDiv.classList.add('post')
-
-//     outerDiv.innerHTML = `<div class="image">
-//                             <img src="${postObject.memeUrl}">
-//                         </div>
-//                         <h4 class="description">${postObject.description}</h4>`
-
-//     listings.append(outerDiv)
 // })
