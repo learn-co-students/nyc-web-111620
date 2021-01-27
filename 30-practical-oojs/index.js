c519d70f9d11f4caca9b4e351a4dbe700eab6e80
Michelle Rios <brendamichelle995@gmail.com>
2021-01-27 00:06:46 -0500

    before lecture

---
+++ 30-practical-oojs/index.js
@@ -0,0 +1,31 @@
+const greg = {
+    firstName: "Greg",
+    lastName: "Dwyer"
+}
+
+const rianna = {
+    firstName: "Rianna",
+    lastName: "Cleary"
+}
+
+const michelle = {
+    firstName: "Michelle",
+    familyName: "Rios"
+}
+
+
+
+// console.log(greg.introduce())
+// console.log(rianna.introduce())
+// console.log(michelle.introduce())
+
+
+// console.log(michelle.introduce)
+// console.log(greg.introduce)
+
+// TRUE or FALSE?
+// console.log(michelle.introduce() === greg.introduce())
+
+
+// TRUE or FALSE?
+// console.log(michelle.introduce === greg.introduce)
