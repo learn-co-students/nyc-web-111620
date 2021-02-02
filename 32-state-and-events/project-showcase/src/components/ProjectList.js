f8e8551f3fd7ba0b16258b28615d65b1bc9962e8
ihollander <ianholla@gmail.com>
2021-02-02 11:47:44 -0500

    After state and events

--- 32-state-and-events/project-showcase/src/components/ProjectList.js
+++ 32-state-and-events/project-showcase/src/components/ProjectList.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectItem from "./ProjectItem";
 // TODO: replace this with a fetch request (eventually)
 import projects from "../data/projects";
 
 function ProjectList() {
-  const projectItems = projects.map((project) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // assembly line
+  // [{}, {}, {}] => [{}]
+  const filteredProjects = projects.filter((project) => {
+    return project.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  console.log(filteredProjects);
+
+  const projectItems = filteredProjects.map((project) => {
     return <ProjectItem key={project.id} project={project} />;
   });
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
 
   return (
     <section>
@@ -20,9 +34,16 @@ function ProjectList() {
         <button>Phase 2</button>
         <button>Phase 1</button>
       </div>
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." onChange={handleChange} />
 
-      <ul className="cards">{projectItems}</ul>
+      <h3>
+        Displaying {filteredProjects.length} of {projects.length} items
+      </h3>
+      {filteredProjects.length > 0 ? (
+        <ul className="cards">{projectItems}</ul>
+      ) : (
+        <h4>No projects found!</h4>
+      )}
     </section>
   );
 }
