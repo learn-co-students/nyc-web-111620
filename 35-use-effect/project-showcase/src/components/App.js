aa82d13f7e7e0f2000df9eca3f2906e912dc99b9
ihollander <ianholla@gmail.com>
2021-02-05 10:25:11 -0500

    mid lecture

--- 35-use-effect/project-showcase/src/components/App.js
+++ 35-use-effect/project-showcase/src/components/App.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 // TODO: replace this with a fetch request (eventually)
-import projectData from "../data/projects";
+// import projectData from "../data/projects";
 
+// App() -> fetch() -> setProjects() -> App() -> fetch() -> setProjects()
+
+// main effect: return JSX (render something to the DOM)
+// side effects: fetch some projects (set a timer; update something outside of the DOM)
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [projects, setProjects] = useState(projectData);
+  const [projects, setProjects] = useState([]);
+
+  // 1st arg: callback function that will run AFTER the component renders
+  // 2nd arg: [] dependencies array: tell React WHEN to run this effect
+  useEffect(() => {
+    console.log("use effect");
+    fetch("http://localhost:4000/projects")
+      .then((r) => r.json())
+      .then((projectArray) => {
+        console.log(projectArray);
+        setProjects(projectArray);
+      });
+  }, []);
+
+  // useEffect(() => {}, [])
+
+  console.log("render");
 
   function handleToggleDarkMode() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
