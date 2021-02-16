bd33e0fe77c016290883e30329fe52aab9ee36c9
ihollander <ianholla@gmail.com>
2021-02-16 11:13:01 -0500

    Got basic example working

--- 39-maps/pokemapper/src/components/PokemonMap.js
+++ 39-maps/pokemapper/src/components/PokemonMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
 import { getRandomPokemon } from "../api/pokemon";
 
 getRandomPokemon().then(console.log);
@@ -9,9 +10,21 @@ function PokemonMap({ position, pokemons }) {
   // display all pokemon in the map
   // when the map is clicked, get a new pokemon and add to state
 
+  // Important! Always set the container height explicitly
   return (
     <main>
-      <h1>Map Goes Here</h1>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+          defaultCenter={{
+            lat: 59.95,
+            lng: 30.33,
+          }}
+          defaultZoom={11}
+        >
+          {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+        </GoogleMapReact>
+      </div>
     </main>
   );
 }
